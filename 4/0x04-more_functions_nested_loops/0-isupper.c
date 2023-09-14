d2d7416c90d08446dfac1022d94bafcf40e402e8
root <root@DESKTOP-MTKU4JI>
2023-09-14 16:56:30 +0100

    0

---
+++ 4/0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - causes an infinite loop
+* description : check if char is upper case
+* Return: 0
+*/
+
+int main(void)
+{
+char c;
+
+scanf("%c", &c);
+if (isupper(c))
+printf("1\n");
+else
+printf("0\n");
+return (0);
+}
