d3077fc33501cace21d651807f9eeb9d3a17a7fd
Soros0 <140617613+Soros0@users.noreply.github.com>
2023-09-25 15:10:39 +0100

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <unistd.h>
 #include <stdio.h>
 
 char *_memset(char *s, char b, unsigned int n);
@@ -12,7 +12,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-
 
 #endif
