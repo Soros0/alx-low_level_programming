80e5c9e0ed9b2e7c743528014b775a41a9e731fc
root <root@DESKTOP-MTKU4JI>
2023-10-12 15:10:30 +0100

    0

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef_VARIAFIC_H_
+#define _VARIADIC_H_
+
+int _putchar(char c);
+int sum_htem_all(const unsigned int n, ...);
+void print_numbers(const char *seperator, cont unsigned int i, ...);
+void print_strings(comst char *seperator, const unsigned int n, ...);
+coid print_all(const char * const format, ...);
+#endif
